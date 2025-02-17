4299116adb2ffca70d94b3e818afed9ebf908c9a
lsmin3388 <lsmin3388_@naver.com>
2025-02-17 17:43:08 +0900

    fix

--- app/src/main/java/com/winter/happyaging/data/aiAnalysis/model/RoomRequest.kt
+++ app/src/main/java/com/winter/happyaging/data/aiAnalysis/model/RoomRequest.kt
@@ -1,7 +1,7 @@
 package com.winter.happyaging.data.aiAnalysis.model
 
 data class RoomRequest(
-    val roomName: String,         // 방 이름
-    val roomCategory: String,      // 방 카테고리
-    val roomImages: List<String>   // 이미지 파일 이름 (백엔드에 업로드한 후 반환된 URL 또는 ID)
+    val roomName: String,
+    val roomCategory: String,
+    val roomImages: List<String>
 )
